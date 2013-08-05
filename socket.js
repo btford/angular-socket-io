6f11cbc6607d7179275f50571069b98a2fc09df8
E John Feig <ejohn@feigdev.com>
2013-08-05 14:11:34 -0700

    added connect and send methods

--- socket.js
+++ socket.js
@@ -7,77 +7,91 @@
 'use strict';
 
 angular.module('btford.socket-io', []).
-  provider('socket', function () {
+    provider('socket', function () {
 
-    // when forwarding events, prefix the event name
-    var prefix = 'socket:',
-      ioSocket;
+        // when forwarding events, prefix the event name
+        var prefix = 'socket:',
+            ioSocket,
+        // removed definition, because we need to connect to the proper address
+            socket;
 
-    // expose to provider
-    this.$get = function ($rootScope, $timeout) {
+        // expose to provider
+        this.$get = function ($rootScope, $timeout) {
 
-      var socket = ioSocket || io.connect();
+            var asyncAngularify = function (callback) {
+                return function () {
+                    var args = arguments;
+                    $timeout(function () {
+                        callback.apply(socket, args);
+                    }, 0);
+                };
+            };
 
-      var asyncAngularify = function (callback) {
-        return function () {  
-          var args = arguments;
-          $timeout(function () {
-            callback.apply(socket, args);
-          }, 0);
+            var addListener = function (eventName, callback) {
+                socket.on(eventName, asyncAngularify(callback));
+            };
+
+            var wrappedSocket = {
+                on: addListener,
+                addListener: addListener,
+
+                connect: function (address) {
+                    socket = ioSocket || io.connect(address);
+                    return this;
+                },
+
+                emit: function (eventName, data, callback) {
+                    if (callback) {
+                        socket.emit(eventName, data, asyncAngularify(callback));
+                    } else {
+                        socket.emit(eventName, data);
+                    }
+                },
+
+                send: function (data, callback) {
+                    if (callback) {
+                        socket.send(data, asyncAngularify(callback));
+                    } else {
+                        socket.send(data);
+                    }
+                },
+
+                removeListener: function () {
+                    var args = arguments;
+                    return socket.removeListener.apply(socket, args);
+                },
+
+                // when socket.on('someEvent', fn (data) { ... }),
+                // call scope.$broadcast('someEvent', data)
+                forward: function (events, scope) {
+                    if (events instanceof Array === false) {
+                        events = [events];
+                    }
+                    if (!scope) {
+                        scope = $rootScope;
+                    }
+                    events.forEach(function (eventName) {
+                        var prefixed = prefix + eventName;
+                        var forwardEvent = asyncAngularify(function (data) {
+                            scope.$broadcast(prefixed, data);
+                        });
+                        scope.$on('$destroy', function () {
+                            socket.removeListener(eventName, forwardEvent);
+                        });
+                        socket.on(eventName, forwardEvent);
+                    });
+                }
+            };
+
+            return wrappedSocket;
         };
-      };
 
-      var addListener = function (eventName, callback) {
-        socket.on(eventName, asyncAngularify(callback));
-      };
+        this.prefix = function (newPrefix) {
+            prefix = newPrefix;
+        };
 
-      var wrappedSocket = {
-        on: addListener,
-        addListener: addListener,
+        this.ioSocket = function (socket) {
+            ioSocket = socket;
+        };
 
-        emit: function (eventName, data, callback) {
-          if (callback) {
-            socket.emit(eventName, data, asyncAngularify(callback));
-          } else {
-            socket.emit(eventName, data);
-          }
-        },
-
-        removeListener: function () {
-          var args = arguments;
-          return socket.removeListener.apply(socket, args);
-        },
-
-        // when socket.on('someEvent', fn (data) { ... }),
-        // call scope.$broadcast('someEvent', data)
-        forward: function (events, scope) {
-          if (events instanceof Array === false) {
-            events = [events];
-          }
-          if (!scope) {
-            scope = $rootScope;
-          }
-          events.forEach(function (eventName) {
-            var prefixed = prefix + eventName;
-            var forwardEvent = asyncAngularify(function (data) {
-              scope.$broadcast(prefixed, data);
-            });
-            scope.$on('$destroy', function () {
-              socket.removeListener(eventName, forwardEvent);
-            });
-            socket.on(eventName, forwardEvent);
-          });
-        }
-      };
-
-      return wrappedSocket;
-    };
-
-    this.prefix = function (newPrefix) {
-      prefix = newPrefix;
-    };
-
-    this.ioSocket = function (socket) {
-      ioSocket = socket;
-    };
-  });
+    });
