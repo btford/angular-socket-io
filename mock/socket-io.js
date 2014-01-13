8c7453f2a51856004e21da1de84d46b357049a71
Tosoa <tosoa@tag-ip.com>
2014-01-13 16:53:49 +0300

    added removeAllListeners and updated removeListener
    copied from socket.io-client v0.9.16

--- mock/socket-io.js
+++ mock/socket-io.js
@@ -6,14 +6,51 @@ function createMockSocketObject () {
 
   var socket = {
     on: function (ev, fn) {
-      this._listeners[ev] = fn;
+      if (!this._listeners) {
+        this._listeners = {};
+      }
+
+      if (!this._listeners[ev]) {
+        this._listeners[ev] = fn;
+      } else if (io.util.isArray(this._listeners[ev])) {
+        this._listeners[ev].push(fn);
+      } else {
+        this._listeners[ev] = [this._listeners[ev], fn];
+      }
     },
     emit: function (ev, data) {
       return (this._listeners[ev] || angular.noop)(data);
     },
-    _listeners: {},
-    removeListener: function (ev) {
-      delete this._listeners[ev];
+    removeListener: function (ev, fn) {
+      if (this._listeners && this._listeners[ev]) {
+        var list = this._listeners[ev];
+
+        if (io.util.isArray(list)) {
+          var pos = -1;
+
+          for (var i = 0, l = list.length; i < l; i++) {
+            if (list[i] === fn || (list[i].listener && list[i].listener === fn)) {
+              pos = i;
+              break;
+            }
+          }
+
+          if (pos < 0) {
+            return this;
+          }
+
+          list.splice(pos, 1);
+
+          if (!list.length) {
+            delete this._listeners[ev];
+          }
+        } else if (list === fn || (list.listener && list.listener === fn)) {
+          delete this._listeners[ev];
+        }
+      }
+    },
+    removeAllListeners: function (ev) {
+      this._listeners[ev] = null;
     }
   };
 
