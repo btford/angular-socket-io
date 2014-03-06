30a5ecee67bec67b62e6be0c7d0688c70b9f1f8c
Gias Kay Lee <balancetraveller+github@gmail.com>
2014-03-07 06:44:43 +0800

    Add uglified version

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    uglify = require('gulp-uglify'),
+    rename = require("gulp-rename"),
+    ngmin = require('gulp-ngmin');
+
+gulp.task('scripts', function() {
+    return gulp.src('socket.js').
+                pipe(rename('socket.min.js')).
+                pipe(ngmin()).
+                pipe(uglify({
+                    outSourceMap: true
+                })).
+                pipe(gulp.dest('.'));
+});
+
+gulp.task('default', function() {
+    gulp.start('scripts');
+});
