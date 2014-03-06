d261249f58e57be0491ff0e862d134aea53c0aa8
Brian Ford <btford@umich.edu>
2014-03-06 15:35:53 -0800

    v0.4.0

--- socket.js
+++ socket.js
@@ -1,6 +1,6 @@
 /*
  * @license
- * angular-socket-io v0.3.0
+ * angular-socket-io v0.4.0
  * (c) 2014 Brian Ford http://briantford.com
  * License: MIT
  */
--- socket.spec.js
+++ socket.spec.js
@@ -1,5 +1,5 @@
 /*
- * angular-socket-io v0.3.0
+ * angular-socket-io v0.4.0
  * (c) 2014 Brian Ford http://briantford.com
  * License: MIT
  */
